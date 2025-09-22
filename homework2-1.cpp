d72b477567c0f64c7d42aa4e5e0af4ba2d7bfdda
kimrhea <krakrakra.0129@gmail.com>
2025-09-22 14:29:58 +0900

    과제제출

--- homework2-1.cpp
+++ homework2-1.cpp
@@ -1,5 +1,51 @@
 #include "homework2-1.h"
 
 /* TODO IntClac 네임스페이스 및 함수 정의하기 */
+    int add(int a, int b){
+        int c;
+        c=a+b;
+        return c;
+    };
+    int subtract(int a, int b){
+        int c;
+        c=a-b;
+        return c;
+
+    };
+    int multiply(int a, int b){
+        int c;
+        c=a*b;
+        return c;
+
+    };
+    int divide(int a, int b){
+        int c;
+        c=a/b;
+        return c;
+    };
+
 
 /* TODO FloatClac 네임스페이스 및 함수 정의하기 */
+    float add(float a, float b){
+        float c;
+        c=a+b;
+        return c;
+    };
+    float subtract(float a, float b){
+        float c;
+        c=a-b;
+        return c;
+
+    };
+    float multiply(float a, float b){
+        float c;
+        c=a*b;
+        return c;
+
+    };
+    float divide(float a, float b){
+        float c;
+        c=a/b;
+        return c;
+
+    };
